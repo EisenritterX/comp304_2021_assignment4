7ff915d4744d708d6f874dd6da4326b82269c912
George Zhou <georgezhou89@hotmail.com>
2021-11-02 11:42:05 -0400

    Added Main Activity and list Selection

--- app/src/main/java/com/example/assignment4/MainActivity.java
+++ app/src/main/java/com/example/assignment4/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.assignment4;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent intent;
+    private ArrayList<String> activities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Find the list view
+        ListView lstActivities = (ListView) findViewById(R.id.listActivities);
+
+        activities = new ArrayList<String>(Arrays.asList(getResources().
+                getStringArray(R.array.Activities)));
+
+
+        // POPULATE LIST
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> activitiesArrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, activities
+        );
+
+        // Attaching ArrayAdapter to list view
+        lstActivities.setAdapter(activitiesArrayAdapter);
+
+        // Attaching event lister on list view
+        lstActivities.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = activities.get(position);
+                // Debug Toast
+                Toast.makeText(getApplicationContext(), "Activities Selected : " +
+                        position + selectedItem, Toast.LENGTH_SHORT).show();
+                // Sent Intent
+                switch (position) {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), ClassroomActivity.class);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(), StudentActivity.class);
+                        break;
+                    case 2:
+                        intent = new Intent(getApplicationContext(), StudentActivity.class);
+                        break;
+                    case 3:
+                        intent = new Intent(getApplicationContext(), ClassroomActivity.class);
+                        break;
+                    case 4:
+                        intent = new Intent(getApplicationContext(), ViewClassroomInfoActivity.class);
+                        break;
+                    case 5:
+                        intent = new Intent(getApplicationContext(), UpdateInfoActivity.class);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
 }
